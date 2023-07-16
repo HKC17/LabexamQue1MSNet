829c6ae4bf3dcebc805f4400c269a7b754b03dcc
Harsh <harshkc17@gmail.com>
2023-07-16 13:20:40 +0530

    Add project files.

---
+++ Labexam/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Labexam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "D:\\cdac_tvm\\7 .net\\Labdemo.txt";
+            string fileContent = "Hello and Welcome\nIt is the first content\nof the text file mytest.txt";
+
+
+            File.WriteAllText(filePath, fileContent);
+
+
+            string readFileContent = File.ReadAllText(filePath);
+
+
+            Console.WriteLine($"Here is the content of the file {filePath}:");
+            Console.WriteLine(readFileContent);
+
+        }
+    }
+}
